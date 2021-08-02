e4d6b9520edc31d49bf3579fa8c078cc3f89c97b
Gabriel Lemos <lemmos@outlook.com.br>
2021-08-02 11:59:52 -0300

    init front

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  title: 'dark',
+  colors: {
+    background: '#202020',
+    mediumBackground: '#333533',
+    title: '#ffd100',
+    subtitle: '#ffee32',
+    primary: '#f8f9fa',
+  },
+};
+
+export type Theme = typeof theme;
